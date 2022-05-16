9dd30090df52383c6d21cc1ea458fc98d1855293
contug <66022609+contug@users.noreply.github.com>
2022-05-16 17:33:56 +0200

    aggiunta di classi universali: .router-element .root-component-container .color-dark .color-medium .color-light .box-shadow

--- src/app/gestione-tavolo/gestione-tavolo.component.ts
+++ src/app/gestione-tavolo/gestione-tavolo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gestione-tavolo',
+  host: {class: 'router-element'},
   templateUrl: './gestione-tavolo.component.html',
   styleUrls: ['./gestione-tavolo.component.css']
 })
