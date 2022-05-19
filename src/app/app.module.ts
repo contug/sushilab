2124319148df6fa10b76ad2443558ccce3f59414
contug <66022609+contug@users.noreply.github.com>
2022-05-19 10:03:02 +0200

    refactor struttura progetto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,18 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {RouterModule, Routes} from "@angular/router";
-import { GestioneTavoloComponent } from './gestione-tavolo/gestione-tavolo.component';
+import { GestioneTavoloComponent } from './components/gestione-tavolo/gestione-tavolo.component';
 import {MatSidenavModule} from "@angular/material/sidenav";
-import { DrawerComponent } from './drawer/drawer.component';
+import { DrawerComponent } from './components/drawer/drawer.component';
 import {MatDividerModule} from "@angular/material/divider";
-import { MenuComponent } from './menu/menu.component';
+import { MenuComponent } from './components/menu/menu.component';
 import {MatCardModule} from "@angular/material/card";
-import { UniscitiComponent } from './unisciti/unisciti.component';
-import { CreaSessioneComponent } from './crea-sessione/crea-sessione.component';
-import { LoginComponent } from './login/login.component';
-import { RegistraComponent } from './registra/registra.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
-import { RecoverPasswordComponent } from './recover-password/recover-password.component';
+import { UniscitiComponent } from './components/unisciti/unisciti.component';
+import { CreaSessioneComponent } from './components/crea-sessione/crea-sessione.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistraComponent } from './components/registra/registra.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { RecoverPasswordComponent } from './components/recover-password/recover-password.component';
 import {MatIconModule} from "@angular/material/icon";
 import {MatExpansionModule} from "@angular/material/expansion";
 
@@ -30,7 +30,7 @@ const routes: Routes = [
   {path: "login", component: LoginComponent},
   {path: "login/registra", component: RegistraComponent},
   {path: "login/forgot-password", component:ForgotPasswordComponent},
-  {path: "login/forgot-password/recover-password",component:RecoverPasswordComponent}
+  {path: "login/forgot-password/recover-password",component:RecoverPasswordComponent},
   {path: "login", component: LoginComponent},
   {path: "login/registra", component: RegistraComponent}
 ]
