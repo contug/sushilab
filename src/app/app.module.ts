097e50481ce7275ee1b437e5b79dee0eec31e9d1
contug <66022609+contug@users.noreply.github.com>
2022-06-06 10:21:02 +0200

    aggiunta area-personale component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import{MatIconModule} from '@angular/material/icon';
+import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterModule, Routes} from "@angular/router";
-import { GestioneTavoloComponent } from './components/gestione-tavolo/gestione-tavolo.component';
+import {GestioneTavoloComponent} from './components/gestione-tavolo/gestione-tavolo.component';
 import {MatSidenavModule} from "@angular/material/sidenav";
-import { DrawerComponent } from './components/drawer/drawer.component';
+import {DrawerComponent} from './components/drawer/drawer.component';
 import {MatDividerModule} from "@angular/material/divider";
-import { MenuComponent } from './components/menu/menu.component';
+import {MenuComponent} from './components/menu/menu.component';
 import {MatCardModule} from "@angular/material/card";
-import { ListaOrdiniComponent } from './components/lista-ordini/lista-ordini.component';
-import { TuoiOrdiniComponent } from './components/tuoi-ordini/tuoi-ordini.component';
+import {ListaOrdiniComponent} from './components/lista-ordini/lista-ordini.component';
+import {TuoiOrdiniComponent} from './components/tuoi-ordini/tuoi-ordini.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import { UniscitiComponent } from './components/unisciti/unisciti.component';
-import { CreaSessioneComponent } from './components/crea-sessione/crea-sessione.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegistraComponent } from './components/registra/registra.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { RecoverPasswordComponent } from './components/recover-password/recover-password.component';
+import {UniscitiComponent} from './components/unisciti/unisciti.component';
+import {CreaSessioneComponent} from './components/crea-sessione/crea-sessione.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegistraComponent} from './components/registra/registra.component';
+import {ForgotPasswordComponent} from './components/forgot-password/forgot-password.component';
+import {RecoverPasswordComponent} from './components/recover-password/recover-password.component';
 import {MatExpansionModule} from "@angular/material/expansion";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MenuService} from "./core/services/menu.service";
-import { GuidaComponent } from './components/guida/guida.component';
-import { InArrivoComponent } from './components/in-arrivo/in-arrivo.component';
-import { PreferitiComponent } from './components/preferiti/preferiti.component';
-import { BlacklistComponent } from './components/blacklist/blacklist.component';
+import {GuidaComponent} from './components/guida/guida.component';
+import {InArrivoComponent} from './components/in-arrivo/in-arrivo.component';
+import {PreferitiComponent} from './components/preferiti/preferiti.component';
+import {BlacklistComponent} from './components/blacklist/blacklist.component';
 import {AuthService} from "./core/auth/auth.service";
 import {AuthInterceptor} from "./core/auth/auth.interceptor";
-import { DialogComponent } from './components/dialog/dialog.component';
+import {DialogComponent} from './components/dialog/dialog.component';
 import {MatDialog, MatDialogModule} from "@angular/material/dialog";
 import {QRCodeModule} from "angularx-qrcode";
+import {AreaPersonaleComponent} from './components/area-personale/area-personale.component';
 
 const routes: Routes = [
   {path: "", redirectTo: "gestione-tavolo", pathMatch: "full"},
@@ -44,18 +45,20 @@ const routes: Routes = [
   {path: "gestione-tavolo/crea-sessione", component: CreaSessioneComponent},
   {path: "login", component: LoginComponent},
   {path: "login/registra", component: RegistraComponent},
-  {path: "login/forgot-password", component:ForgotPasswordComponent},
-  {path: "login/forgot-password/recover-password",component:RecoverPasswordComponent},
-  {path: "lista-ordini", component:ListaOrdiniComponent},
-  {path: "login/forgot-password/recover-password",component:RecoverPasswordComponent},
+  {path: "login/forgot-password", component: ForgotPasswordComponent},
+  {path: "login/forgot-password/recover-password", component: RecoverPasswordComponent},
+  {path: "lista-ordini", component: ListaOrdiniComponent},
+  {path: "login/forgot-password/recover-password", component: RecoverPasswordComponent},
   {path: "login", component: LoginComponent},
   {path: "login/registra", component: RegistraComponent},
   {path: "tuoi-ordini", component: TuoiOrdiniComponent},
-  {path: "in-arrivo", component:InArrivoComponent},
-  {path: "preferiti", component:PreferitiComponent},
+  {path: "in-arrivo", component: InArrivoComponent},
+  {path: "preferiti", component: PreferitiComponent},
   {path: "guida", component: GuidaComponent},
-  {path: "blacklist", component: BlacklistComponent}
+  {path: "blacklist", component: BlacklistComponent},
+  {path: "area-personale", component: AreaPersonaleComponent}
 ]
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -75,6 +78,7 @@ const routes: Routes = [
     PreferitiComponent,
     BlacklistComponent,
     DialogComponent,
+    AreaPersonaleComponent,
   ],
   imports: [
     BrowserModule,
@@ -104,4 +108,5 @@ const routes: Routes = [
     DialogComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/components/area-personale/area-personale.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-area-personale',
+  host: {class: 'router-element'},
+  templateUrl: './area-personale.component.html',
+  styleUrls: ['./area-personale.component.css']
+})
+export class AreaPersonaleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
