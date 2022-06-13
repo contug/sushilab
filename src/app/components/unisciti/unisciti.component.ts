7b4d6cab34252bb9217e6b4bb66eaf5fb3805238
contug <66022609+contug@users.noreply.github.com>
2022-06-13 16:09:02 +0200

    qr scanner completo

--- src/app/components/unisciti/unisciti.component.ts
+++ src/app/components/unisciti/unisciti.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {QrDialogComponent} from "../qr-dialog/qr-dialog.component";
 import {QrService} from "../../core/services/qr.service";
 import {SessioneHttpService} from "../../core/http/sessione-http.service";
 import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validator, Validators} from "@angular/forms";
+import {resolve} from "@angular/compiler-cli";
 
 @Component({
   selector: 'app-unisciti',
@@ -13,15 +15,20 @@ import {Router} from "@angular/router";
 })
 export class UniscitiComponent implements OnInit {
 
+  qrForm!: FormGroup;
+
   constructor(public dialog: MatDialog,
               public qrService: QrService,
               public sessioneHttpService: SessioneHttpService,
-              private router: Router) { }
+              private router: Router,
+              private fb: FormBuilder) {
+    this.qrForm = this.fb.group({'qrInput': ['', Validators.required]})
+  }
 
   ngOnInit(): void {
   }
 
-  openDialog() : void {
+  openDialog(): void {
     const dialogRef = this.dialog.open(QrDialogComponent, {
       width: '75vw',
       data: {scannedCode: this.qrService.qrCode},
@@ -29,15 +36,42 @@ export class UniscitiComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       console.log("qrdialog chiuso")
-      this.sessioneHttpService.ottieniSessione(this.qrService.qrCode).subscribe(res => {
-        if(res == null || res == undefined) {
-          alert("Questo QR non è valido!")
-          this.qrService.clean();
-        }
-        else
-          this.router.navigateByUrl("/menu");
-      })
+      if (this.qrService.qrCode != '') {
+        console.log('qr dopo chiusura dialog: ' + this.qrService.qrCode)
+        this.sessioneHttpService.checkSessione(this.qrService.qrCode).subscribe(res => {
+          console.log("log checkSessione")
+          console.log(res.status)
+          if (res.status != 200) {
+            console.log("qr non valido")
+            alert("Questo QR non è valido!")
+            this.qrService.clean();
+          } else
+            this.router.navigateByUrl("/menu");
+        })
+      }
+
+
     })
+  }
+
+  onClickUnisciti() {
+    let validator = this.qrForm.value
+    console.log("qr manuale: " + validator.qrInput)
+    if (validator.qrInput) {
+      this.sessioneHttpService.checkSessione(validator.qrInput).subscribe(res => {
+        console.log(res.status)
+        console.log(res.body)
+        if (res.status != 200) {
+          alert("Questo QR non è valido!")
+        }
+        else {
+          this.qrService.qrCode = validator.qrInput;
+          console.log('qr manuale nel servizio: ' + this.qrService.qrCode)
+          this.router.navigateByUrl("/menu");
+        }
+      })
+
+    }
   }
 
 
--- src/app/core/http/sessione-http.service.ts
+++ src/app/core/http/sessione-http.service.ts
@@ -21,6 +21,10 @@ export class SessioneHttpService {
     return this.http.get<Tavolo>(Constants.ROOT_URL+'/tavolo/' + id);
   }
 
+  checkSessione(id: string) : Observable<any> {
+    return this.http.get<any>(Constants.ROOT_URL + '/tavolo/' + id, {observe: "response"});
+  }
+
   chiudiSessione(id: string):void {
     this.http.delete(Constants.ROOT_URL +'/tavolo/' + id);
   }
